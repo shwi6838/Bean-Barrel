aa5a123ac8bf6db89d4eecced5f619513a4bdfc5
u8450 <ronaguo0901@gmail.com>
2025-04-09 16:09:18 -0600

    new api key

--- Client/src/Pages/Map.jsx
+++ Client/src/Pages/Map.jsx
@@ -3,9 +3,16 @@ import React from "react";
 function MapPage() {
     return (
         <div>
-        <h1>Map</h1>
+            <h1>Map</h1>
+            <iframe 
+                title= 'Search result for Cafes in Boulder, CO' 
+                width="600" height="450" style="border:0" loading="lazy" 
+                allowfullscreen= ''
+                src="https://www.google.com/maps/embed/v1/search?q=cafe&key=AIzaSyA70Gar_IcD8ukAkW1bOsVuniZEddlPAqM">
+            </iframe>
         </div>
     );
 }
+
 
 export default MapPage
