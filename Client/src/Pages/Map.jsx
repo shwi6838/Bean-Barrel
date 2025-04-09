1fe458dcc71a6be37c462fc36b3e2346bef688d1
HMQ <hmqf9819@gmail.com>
2025-04-09 16:23:38 -0600

    Updated map page, can see the map

--- Client/src/Pages/Map.jsx
+++ Client/src/Pages/Map.jsx
@@ -2,17 +2,18 @@ import React from "react";
 
 function MapPage() {
     return (
-        <div>
-            <h1>Map</h1>
-            <iframe 
-                title= 'Search result for Cafes in Boulder, CO' 
-                width="600" height="450" style="border:0" loading="lazy" 
-                allowfullscreen= ''
-                src="https://www.google.com/maps/embed/v1/search?q=cafe&key=AIzaSyA70Gar_IcD8ukAkW1bOsVuniZEddlPAqM">
-            </iframe>
+        <div style={{ height: "79vh", width: "100vw", margin: 0, padding: 0 }}>
+            <iframe
+                title="Search result for Cafes in Boulder, CO"
+                width="100%"
+                height="100%"
+                style={{ border: 0 }}
+                loading="lazy"
+                allowFullScreen
+                src={`https://www.google.com/maps/embed/v1/search?q=cafe&key=AIzaSyA70Gar_IcD8ukAkW1bOsVuniZEddlPAqM`}
+            ></iframe>
         </div>
     );
 }
 
-
-export default MapPage
+export default MapPage;
