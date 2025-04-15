e85c255307e7ef57532cdb0bc9bd40e659d52def
u8450 <ronaguo0901@gmail.com>
2025-04-15 15:15:50 -0600

    new map page

--- Client/src/Pages/Map.jsx
+++ Client/src/Pages/Map.jsx
@@ -6,15 +6,13 @@ const position = { lat: 40.015, lng: -105.2705 }; // Boulder, CO
 
 function MapPage() {
     const apiKey = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
-    GOOGLE_MAPS_API_KEY
     return (
         <div style={{ height: "79vh", width: "100vw", margin: 0, padding: 0 }}>
             <p title="Search result for Cafes in Boulder, CO" />
             <APIProvider apiKey = {apiKey}>
                 <Map
-    
-                    width="100%"
-                    height="100%"
+                    width="50%"
+                    height="50%"
                     defaultCenter={position}
                     defaultZoom={13}
                     style={{ border: 0 }}
